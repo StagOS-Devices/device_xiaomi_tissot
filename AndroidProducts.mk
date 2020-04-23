51bcb84f97ec073c7de8ffe20f56835c67ad9557
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-04-23 16:17:57 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tissot.mk
+    $(LOCAL_DIR)/stag_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_tissot-user \
-    derp_tissot-userdebug \
-    derp_tissot-eng \
+    stag_tissot-user \
+    stag_tissot-userdebug \
+    stag_tissot-eng \
